64f92818eb719adac86e2af51910013a16ea63d6
kyleyiyang <kyleyiyang@gmail.com>
2021-01-07 23:03:40 -0500

    Update maxSubArray.java

--- maxSubArray.java
+++ maxSubArray.java
@@ -1,22 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int min=nums[0];
-        int ans=nums[0];
-        for (int i=0;i<nums.length;i++) {
-            int temp=nums[i];
-            int j=i+1;
-            while (j<nums.length && nums[j]>=min) {
-                temp+=nums[j];
-                j++;
-                System.out.println("j="+j);
-            }
-            if (temp>ans) ans=temp;
-            if (j<nums.length) {
-                min=nums[j];
-                i=j;
-                System.out.println("j="+j);
-            }
+        int maxSoFar=nums[0], maxEndingHere=nums[0];
+        for (int i=1;i<nums.length;i++) {
+            maxEndingHere= Math.max(maxEndingHere+nums[i],nums[i]);
+            maxSoFar=Math.max(maxSoFar, maxEndingHere);	
         }
-        return ans;
+        return maxSoFar;
     }
 }
